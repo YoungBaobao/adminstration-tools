811bd46623b3c141ed26d9dfa5a04adf7f7f00e1
Vekaco <dr.vekaco@gmail.com>
2019-09-09 21:39:26 +0800

    project structure

---
+++ src/main/java/com/yangbaobao/administrationtools/AdministrationToolsApplication.java
@@ -0,0 +1,13 @@
+package com.yangbaobao.administrationtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrationToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministrationToolsApplication.class, args);
+	}
+
+}
